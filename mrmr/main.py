6ef571a56ea84bcb2fea8f0579baa2dd9471c265
smazzanti <41154179+smazzanti@users.noreply.github.com>
2021-04-21 23:12:20 +0200

    add support for regression problems

--- mrmr/main.py
+++ mrmr/main.py
@@ -2,8 +2,9 @@ from joblib import Parallel, delayed
 from multiprocessing import cpu_count
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.feature_selection import f_classif as sklearn_f_classif
+from sklearn.feature_selection import f_regression as sklearn_f_regression
 import warnings; warnings.filterwarnings("ignore")
 
 FLOOR = .00001
@@ -31,6 +32,14 @@ def _f_classif(X, y):
         return sklearn_f_classif(x[x_not_na].to_frame(), y[x_not_na])[0][0]
     return X.apply(lambda col: _f_classif_series(col, y)).fillna(0.0)
 
+def _f_regression(X, y):
+    def _f_regression_series(x, y):
+        x_not_na = ~ x.isna()
+        if x_not_na.sum() == 0:
+            return 0
+        return sklearn_f_regression(x[x_not_na].to_frame(), y[x_not_na])[0][0]
+    return X.apply(lambda col: _f_regression_series(col, y)).fillna(0.0)
+
 def _corr_pearson(A, b):
     return A.corrwith(b).fillna(0.0).abs().clip(FLOOR)
 
@@ -42,9 +51,18 @@ def f_classif(X, y):
     '''Compute F-statistic between DataFrame X and Series y'''
     return parallel_df(_f_classif, X, y)
 
+def f_regression(X, y):
+    '''Compute F-statistic between DataFrame X and Series y'''
+    return parallel_df(_f_regression, X, y)
+
 def random_forest_classif(X, y):
     '''Compute feature importance of each column of DataFrame X after fitting a random forest on Series y'''
     forest = RandomForestClassifier(max_depth = 5, random_state = 0).fit(X.fillna(X.min().min() - 1), y)
+    return pd.Series(forest.feature_importances_, index = X.columns)
+
+def random_forest_regression(X, y):
+    '''Compute feature importance of each column of DataFrame X after fitting a random forest on Series y'''
+    forest = RandomForestRegressor(max_depth = 5, random_state = 0).fit(X.fillna(X.min().min() - 1), y)
     return pd.Series(forest.feature_importances_, index = X.columns)
 
 def corr_pearson(A, b):
@@ -53,42 +71,29 @@ def corr_pearson(A, b):
 
 #####################################################################
 # MRMR selection
-    
-def mrmr_classif(X, y, K, relevance = 'f', redundancy = 'c', denominator = 'mean', only_same_domain = False):
+
+def _mrmr_base(X, y, K, func_relevance, func_redundancy, func_denominator, only_same_domain = False):
     '''
-    Do MRMR selection on a set of data.
+    Do MRMR selection.
     
     Args:
         X: (pandas.DataFrame) A Dataframe consisting of numeric features only.
-        y: (pandas.Series) A Series containing the (categorical) target variable.
+        y: (pandas.Series) A Series containing the target variable.
         K: (int) Number of features to select.
-        relevance: (str) Name of relevance method. Supported: 'f' (f-statistic), 'rf' (random forest).
-        redundancy: (str) Name of redundancy method. Supported: 'c' (Pearson correlation)
-        denominator: (str) Name of synthesis function to apply to the denominator. Supported: 'max', 'mean'
+        func_relevance: (func) Relevance function.
+        func_redundancy: (func) Redundancy function.
+        func_denominator: (func) Synthesis function to apply to the denominator.
         only_same_domain: (bool) If False, all the necessary correlation coefficients are computed.
             If True, only features belonging to the same domain are compared.
             Domain is defined by the string preceding the first underscore:
-            for instance "cusinfo_age" and "cusinfo_income" belong to the same domain,
-            whereas "age" and "income" don't.
+            for instance "cusinfo_age" and "cusinfo_income" belong to the same domain,whereas "age" and "income" don't.
         
     Returns:
         (list) List of K names of selected features (sorted by importance).
     '''
     
-    FUNCS = {
-        'f': f_classif, 
-        'rf': random_forest_classif,
-        'c': corr_pearson,
-        'mean': np.mean,
-        'max': np.max
-    }
-    
-    func_relevance = FUNCS[relevance]
-    func_redundancy = FUNCS[redundancy]
-    func_score_denominator = FUNCS[denominator]
-    
     # compute relevance
-    rel = func_relevance(X, y).round(5)
+    rel = func_relevance(X, y)
     
     # keep only columns that have positive relevance
     columns = rel[rel.fillna(0) > 0].index.to_list()
@@ -117,7 +122,7 @@ def mrmr_classif(X, y, K, relevance = 'f', redundancy = 'c', denominator = 'mean
                                 
             if not_selected_subset:
                 red.loc[not_selected_subset, last_selected] = func_redundancy(X[not_selected_subset], X[last_selected]).abs().clip(FLOOR).fillna(FLOOR)
-                score_denominator = red.loc[not_selected, selected].apply(func_score_denominator, axis = 1).round(5).replace(1.0, float('Inf'))
+                score_denominator = red.loc[not_selected, selected].apply(func_denominator, axis = 1).round(5).replace(1.0, float('Inf'))
                 
         else:
             score_denominator = pd.Series(1, index = columns)
@@ -129,3 +134,85 @@ def mrmr_classif(X, y, K, relevance = 'f', redundancy = 'c', denominator = 'mean
         not_selected.remove(best)
         
     return selected
+    
+def mrmr_classif(X, y, K, relevance = 'f', redundancy = 'c', denominator = 'mean', only_same_domain = False):
+    '''
+    Do MRMR feature selection on classification task.
+    
+    Args:
+        X: (pandas.DataFrame) A Dataframe consisting of numeric features only.
+        y: (pandas.Series) A Series containing the (categorical) target variable.
+        K: (int) Number of features to select.
+        relevance: (str or function) Relevance method. 
+            If function, it should take X and y as input and return a pandas.Series containing a (non-negative) score of relevance for each feature of X.
+            If string, name of method, supported: 'f' (f-statistic), 'rf' (random forest).
+        redundancy: (str or function) Redundancy method. 
+            If function, it should take A and b as input and return a pandas.Series containing a (non-negative) score of redundancy for each feature of A.
+            If string, name of method, supported: 'c' (Pearson correlation)
+        denominator: (str or function) Synthesis function to apply to the denominator of MRMR score.
+            If function, it should take an iterable as input and return a scalar.
+            If string, name of method, supported: 'max', 'mean'
+        only_same_domain: (bool) If False, all the necessary correlation coefficients are computed.
+            If True, only features belonging to the same domain are compared.
+            Domain is defined by the string preceding the first underscore:
+            for instance "cusinfo_age" and "cusinfo_income" belong to the same domain,
+            whereas "age" and "income" don't.
+        
+    Returns:
+        (list) List of K names of selected features (sorted by importance).
+    '''
+    
+    FUNCS = {
+        'f': f_classif, 
+        'rf': random_forest_classif,
+        'c': corr_pearson,
+        'mean': np.mean,
+        'max': np.max
+    }
+    
+    func_relevance = FUNCS[relevance] if relevance in FUNCS.keys() else relevance
+    func_redundancy = FUNCS[redundancy] if redundancy in FUNCS.keys() else redundancy
+    func_denominator = FUNCS[denominator] if denominator in FUNCS.keys() else denominator
+    
+    return _mrmr_base(X, y, K, func_relevance, func_redundancy, func_denominator, only_same_domain = False)
+        
+def mrmr_regression(X, y, K, relevance = 'f', redundancy = 'c', denominator = 'mean', only_same_domain = False):
+    '''
+    Do MRMR feature selection on regression task.
+    
+    Args:
+        X: (pandas.DataFrame) A Dataframe consisting of numeric features only.
+        y: (pandas.Series) A Series containing the (numerical) target variable.
+        K: (int) Number of features to select.
+        relevance: (str or function) Relevance method. 
+            If function, it should take X and y as input and return a pandas.Series containing a (non-negative) score of relevance for each feature of X.
+            If string, name of method, supported: 'f' (f-statistic), 'rf' (random forest).
+        redundancy: (str or function) Redundancy method. 
+            If function, it should take A and b as input and return a pandas.Series containing a (non-negative) score of redundancy for each feature of A.
+            If string, name of method, supported: 'c' (Pearson correlation)
+        denominator: (str or function) Synthesis function to apply to the denominator of MRMR score.
+            If function, it should take an iterable as input and return a scalar.
+            If string, name of method, supported: 'max', 'mean'
+        only_same_domain: (bool) If False, all the necessary correlation coefficients are computed.
+            If True, only features belonging to the same domain are compared.
+            Domain is defined by the string preceding the first underscore:
+            for instance "cusinfo_age" and "cusinfo_income" belong to the same domain,
+            whereas "age" and "income" don't.
+        
+    Returns:
+        (list) List of K names of selected features (sorted by importance).
+    '''
+    
+    FUNCS = {
+        'f': f_regression, 
+        'rf': random_forest_regression,
+        'c': corr_pearson,
+        'mean': np.mean,
+        'max': np.max
+    }
+    
+    func_relevance = FUNCS[relevance] if relevance in FUNCS.keys() else relevance
+    func_redundancy = FUNCS[redundancy] if redundancy in FUNCS.keys() else redundancy
+    func_denominator = FUNCS[denominator] if denominator in FUNCS.keys() else denominator
+    
+    return _mrmr_base(X, y, K, func_relevance, func_redundancy, func_denominator, only_same_domain = False)
