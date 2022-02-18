412d82fd37a3a46c3378434c16226591c3727fec
smazzanti <mazzanti.sam@gmail.com>
2022-02-19 00:18:34 +0100

    update version to 0.2.1

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", encoding="utf8") as f:
+    long_description = f.read()
+
 setup(
-    name='mrmr',
-    version='0.2.0',
+    name='mrmr_selection',
+    version='0.2.1',
     description='minimum-Redundancy-Maximum-Relevance algorithm for feature selection',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/smazzanti/mrmr',
     author='Samuele Mazzanti',
     author_email='mazzanti.sam@gmail.com',
