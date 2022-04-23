feda16b0f05a1676dc0693d3a2f9d650154c1d3b
smazzanti <mazzanti.sam@gmail.com>
2022-04-23 20:42:28 +0200

    Correct random forest params in mrmr classif

--- mrmr/__init__.py
+++ mrmr/__init__.py
@@ -4,4 +4,4 @@ from . import spark
 from .pandas import mrmr_classif, mrmr_regression
 from .main import mrmr_base
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
--- mrmr/pandas.py
+++ mrmr/pandas.py
@@ -78,6 +78,7 @@ def random_forest_regression(X, y):
 def correlation(target_column, features, X, n_jobs):
     def _correlation(X, y):
         return X.corrwith(y).fillna(0.0)
+
     return parallel_df(_correlation, X.loc[:, features], X.loc[:, target_column], n_jobs=n_jobs)
 
 
@@ -99,62 +100,49 @@ def mrmr_classif(
         n_jobs=-1, show_progress=True
 ):
     """MRMR feature selection for a classification task
-
     Parameters
     ----------
     X: pandas.DataFrame
         A DataFrame containing all the features.
-
     y: pandas.Series
         A Series containing the (categorical) target variable.
-
     K: int
         Number of features to select.
-
     features: list of str (optional, default=None)
         List of numeric column names. If not specified, all numeric columns (integer and float) are used.
-
     relevance: str or callable
         Relevance method.
         If string, name of method, supported: "f" (f-statistic), "ks" (kolmogorov-smirnov), "rf" (random forest).
         If callable, it should take "X" and "y" as input and return a pandas.Series containing a (non-negative)
         score of relevance for each feature.
-
     redundancy: str or callable
         Redundancy method.
         If string, name of method, supported: "c" (Pearson correlation).
         If callable, it should take "X", "target_column" and "features" as input and return a pandas.Series
         containing a score of redundancy for each feature.
-
     denominator: str or callable (optional, default='mean')
         Synthesis function to apply to the denominator of MRMR score.
         If string, name of method. Supported: 'max', 'mean'.
         If callable, it should take an iterable as input and return a scalar.
-
     cat_features: list (optional, default=None)
         List of categorical features. If None, all string columns will be encoded.
-
     cat_encoding: str
         Name of categorical encoding. Supported: 'leave_one_out', 'james_stein', 'target'.
-
     only_same_domain: bool (optional, default=False)
         If False, all the necessary correlation coefficients are computed.
         If True, only features belonging to the same domain are compared.
         Domain is defined by the string preceding the first underscore:
         for instance "cusinfo_age" and "cusinfo_income" belong to the same domain, whereas "age" and "income" don't.
-
     return_scores: bool (optional, default=False)
         If False, only the list of selected features is returned.
         If True, a tuple containing (list of selected features, relevance, redundancy) is returned.
-        
+
     n_jobs: int (optional, default=-1)
         Maximum number of workers to use. Only used when relevance = "f" or redundancy = "corr".
         If -1, use as many workers as min(cpu count, number of features).
-
     show_progress: bool (optional, default=True)
         If False, no progress bar is displayed.
         If True, a TQDM progress bar shows the number of features processed.
-
     Returns
     -------
     selected_features: list of str
@@ -164,18 +152,18 @@ def mrmr_classif(
     if cat_features:
         X = encode_df(X=X, y=y, cat_features=cat_features, cat_encoding=cat_encoding)
 
-    if relevance=="f":
+    if relevance == "f":
         relevance_func = functools.partial(f_classif, n_jobs=n_jobs)
-    elif relevance=="ks":
+    elif relevance == "ks":
         relevance_func = functools.partial(ks_classif, n_jobs=n_jobs)
-    elif relevance=="rf":
+    elif relevance == "rf":
         relevance_func = random_forest_classif
     else:
         relevance_func = relevance
 
     redundancy_func = functools.partial(correlation, n_jobs=n_jobs) if redundancy == 'c' else redundancy
     denominator_func = np.mean if denominator == 'mean' else (
-                       np.max if denominator == 'max' else denominator)
+        np.max if denominator == 'max' else denominator)
 
     relevance_args = {'X': X, 'y': y}
     redundancy_args = {'X': X}
@@ -190,66 +178,53 @@ def mrmr_regression(
         X, y, K,
         relevance='f', redundancy='c', denominator='mean',
         cat_features=None, cat_encoding='leave_one_out',
-        only_same_domain=False, return_scores=False, 
+        only_same_domain=False, return_scores=False,
         n_jobs=-1, show_progress=True
 ):
     """MRMR feature selection for a regression task
-
     Parameters
     ----------
     X: pandas.DataFrame
         A DataFrame containing all the features.
-
     y: pandas.Series
         A Series containing the (categorical) target variable.
-
     K: int
         Number of features to select.
-
     features: list of str (optional, default=None)
         List of numeric column names. If not specified, all numeric columns (integer and float) are used.
-
     relevance: str or callable
         Relevance method.
         If string, name of method, supported: "f" (f-statistic), "rf" (random forest).
         If callable, it should take "X" and "y" as input and return a pandas.Series containing a (non-negative)
         score of relevance for each feature.
-
     redundancy: str or callable
         Redundancy method.
         If string, name of method, supported: "c" (Pearson correlation).
         If callable, it should take "X", "target_column" and "features" as input and return a pandas.Series
         containing a score of redundancy for each feature.
-
     denominator: str or callable (optional, default='mean')
         Synthesis function to apply to the denominator of MRMR score.
         If string, name of method. Supported: 'max', 'mean'.
         If callable, it should take an iterable as input and return a scalar.
-
     cat_features: list (optional, default=None)
         List of categorical features. If None, all string columns will be encoded.
-
     cat_encoding: str
         Name of categorical encoding. Supported: 'leave_one_out', 'james_stein', 'target'.
-
     only_same_domain: bool (optional, default=False)
         If False, all the necessary correlation coefficients are computed.
         If True, only features belonging to the same domain are compared.
         Domain is defined by the string preceding the first underscore:
         for instance "cusinfo_age" and "cusinfo_income" belong to the same domain, whereas "age" and "income" don't.
-
     return_scores: bool (optional, default=False)
         If False, only the list of selected features is returned.
         If True, a tuple containing (list of selected features, relevance, redundancy) is returned.
-        
+
     n_jobs: int (optional, default=-1)
         Maximum number of workers to use. Only used when relevance = "f" or redundancy = "corr".
         If -1, use as many workers as min(cpu count, number of features).
-
     show_progress: bool (optional, default=True)
         If False, no progress bar is displayed.
         If True, a TQDM progress bar shows the number of features processed.
-
     Returns
     -------
     selected_features: list of str
@@ -258,11 +233,11 @@ def mrmr_regression(
     if cat_features:
         X = encode_df(X=X, y=y, cat_features=cat_features, cat_encoding=cat_encoding)
 
-    relevance_func = functools.partial(f_regression, n_jobs=n_jobs) if relevance=='f' else (
-                     random_forest_regression if relevance=='rf' else relevance)
+    relevance_func = functools.partial(f_regression, n_jobs=n_jobs) if relevance == 'f' else (
+        random_forest_regression if relevance == 'rf' else relevance)
     redundancy_func = functools.partial(correlation, n_jobs=n_jobs) if redundancy == 'c' else redundancy
     denominator_func = np.mean if denominator == 'mean' else (
-                       np.max if denominator == 'max' else denominator)
+        np.max if denominator == 'max' else denominator)
 
     relevance_args = {'X': X, 'y': y}
     redundancy_args = {'X': X}
