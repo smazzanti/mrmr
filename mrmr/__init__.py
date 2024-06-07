c04423106a337ec3129a45183b60c991cf6196ab
Henry Webel <henry.webel@cpr.ku.dk>
2024-06-07 14:41:04 +0200

    :art: make it installable from github

--- mrmr/__init__.py
+++ mrmr/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import version
+
 from mrmr import bigquery
 from mrmr import pandas
 from mrmr import polars
@@ -5,4 +7,4 @@ from mrmr import spark
 from mrmr.pandas import mrmr_classif, mrmr_regression
 from mrmr.main import mrmr_base
 
-__version__ = "0.2.8"
+_version__ = version('mrmr_selection')
