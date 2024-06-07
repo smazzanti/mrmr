a1a0475ade2b82674caabeb2edc4871f28787fff
Henry Webel <henry.webel@cpr.ku.dk>
2024-06-07 15:26:15 +0200

    :bug: workaround for polar on M1
    - only load polar if requested explicitly
    - not needed for main function
    
    see: https://github.com/pola-rs/polars/issues/11650
    
    (polars on M1 needs correct compiled binaries)

--- mrmr/__init__.py
+++ mrmr/__init__.py
@@ -2,7 +2,8 @@ from importlib.metadata import version
 
 from mrmr import bigquery
 from mrmr import pandas
-from mrmr import polars
+# trigger: zsh: illegal hardware instructions if used with wrong package
+# from mrmr import polars
 from mrmr import spark
 from mrmr.pandas import mrmr_classif, mrmr_regression
 from mrmr.main import mrmr_base
